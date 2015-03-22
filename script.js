c32bfc18273753c3c1beeaa1ac0a33afad44b324
Laura Knight <ljknight@gmail.com>
2015-03-22 15:06:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+var counter = 0;
 
+$('button').on("click", function() {
+  var $this = $(this);
+  counter += 1;
+  $this.text(counter);
+  var classText;
+  if (counter % 4 === 0) {
+    classText = "zero";
+  } else if (counter % 4 === 1) {
+    classText = "one";
+  } else if (counter % 4 === 2) {
+    classText = "two";
+  } else if (counter % 4 === 3) {
+    classText = "three";
+  }
+  $this.prop("class", classText);
+});
